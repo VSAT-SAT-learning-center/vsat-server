b03d932ac1364d6b2fe6ae879948885738e3d498
Duy Nguyen Manh <108318095+DuyNM0601@users.noreply.github.com>
2024-10-04 19:16:25 +0700

    04/10/24-DuyNM Add API ExamType about Get, Update, Create

--- src/modules/question/question.service.ts
+++ src/modules/question/question.service.ts
@@ -81,16 +81,28 @@ export class QuestionService {
         });
     }
 
-    async getAll(): Promise<GetQuestionDTO[]> {
-        const questions = await this.questionRepository.find({
+    async getAll(page: number, pageSize: number): Promise<any> {
+        const skip = (page - 1) * pageSize;
+
+        const [questions, total] = await this.questionRepository.findAndCount({
             relations: ['unit', 'level', 'skill', 'lesson'],
+            skip: skip,
+            take: pageSize,
         });
-        return plainToInstance(GetQuestionDTO, questions, {
-            excludeExtraneousValues: true,
-        });
+
+        const totalPages = Math.ceil(total / pageSize);
+        return{
+            data: plainToInstance(GetQuestionDTO, questions, {
+                excludeExtraneousValues: true,
+            }),
+            totalPages: totalPages,
+            currentPage: page,
+            totalItems: total,
+
+        } 
     }
 
-    async approve(id: string, status: QuestionStatus): Promise<boolean> {
+    async updateStatus(id: string, status: QuestionStatus): Promise<boolean> {
         if (!Object.values(QuestionStatus).includes(status)) {
             throw new BadRequestException(`Invalid status value: ${status}`);
         }
@@ -159,13 +171,24 @@ export class QuestionService {
         });
     }
 
-    async getQuestionWithAnswer(): Promise<GetQuestionWithAnswerDTO[]> {
-        const questions = await this.questionRepository.find({
+    async getQuestionWithAnswer(page: number, pageSize: number): Promise<any> {
+        const skip = (page - 1) * pageSize;
+
+        const [questions, total] = await this.questionRepository.findAndCount({
             relations: ['answers'],
+            skip: skip,
+            take: pageSize,
         });
 
-        return plainToInstance(GetQuestionWithAnswerDTO, questions, {
-            excludeExtraneousValues: true,
-        });
+        const totalPages = Math.ceil(total / pageSize);
+
+        return {
+            data: plainToInstance(GetQuestionWithAnswerDTO, questions, {
+                excludeExtraneousValues: true,
+            }),
+            totalPages: totalPages,
+            currentPage: page,
+            totalItems: total,
+        };
     }
 }
