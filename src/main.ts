f2bb15fa1efc3fc5f2a47e509fe71467a3972181
devdoubled <duylvd2002@gmail.com>
2024-10-17 12:01:36 +0700

    17/10/24-DuyLVD Add CORS

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 declare const module: any;
 
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/exception/exception.filter';
 
 async function bootstrap() {
@@ -34,10 +34,10 @@ async function bootstrap() {
 
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
-
+    app.enableCors();
     await app.listen(PORT, () => {
         console.log('start success with port: ' + PORT);
-        console.log('Swagger in: ' + 'http://localhost:' + PORT + '/api#/');
+        console.log('Swagger in: ' + 'http://localhost:' + PORT + '/api/v1/');
     });
 
     if (module.hot) {
