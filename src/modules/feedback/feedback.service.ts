73460bbe7bec9ecd563aad10056ed5df8c010dec
Cong Cao <caocong2404@gmail.com>
2024-10-31 08:56:17 +0700

    Merge main into CongCV2 branch

--- src/modules/feedback/feedback.service.ts
+++ src/modules/feedback/feedback.service.ts
@@ -247,19 +247,4 @@ export class FeedbackService extends BaseService<Feedback> {
         // Transform the entity to DTO
         return plainToInstance(QuestionFeedbackResponseDto, feedback, { excludeExtraneousValues: true });
     }
-
-    async getLessonFeedbackUserId(userId: string, questionId: string): Promise<QuestionFeedbackResponseDto> {
-        const feedback = await this.feedbackRepository.findOne({
-            where: [{ accountTo: { id: userId }, question: { id: questionId } }],
-            relations: ['question', 'accountFrom', 'accountTo'],
-            order: { updatedat: 'DESC' },
-        });
-
-        if (!feedback) {
-            throw new Error('Feedback not found');
-        }
-    
-        // Transform the entity to DTO
-        return plainToInstance(QuestionFeedbackResponseDto, feedback, { excludeExtraneousValues: true });
-    }
 }
