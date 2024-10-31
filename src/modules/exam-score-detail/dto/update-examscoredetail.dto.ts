f9d5110bdaa9bc3ff268eae374b8a69b9395076d
Duy Nguyen Manh <108318095+DuyNM0601@users.noreply.github.com>
2024-10-31 23:52:36 +0700

    31/10/24-DuyNM Fix api update exam score detail, update quiz question

--- src/modules/exam-score-detail/dto/update-examscoredetail.dto.ts
+++ src/modules/exam-score-detail/dto/update-examscoredetail.dto.ts
@@ -1,12 +1,24 @@
+import { Expose } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsUUID, IsInt, IsOptional, IsString } from 'class-validator';
 
 export class UpdateExamScoreDetailDto {
+    @IsUUID()
+    @Expose()
+    @ApiProperty()
+    id: string;
+
     @IsInt()
     @ApiProperty()
+    @Expose()
     lowerscore?: number;
 
     @IsInt()
     @ApiProperty()
+    @Expose()
     upperscore?: number;
+
+    @IsString()
+    @ApiProperty()
+    section: string;
 }
