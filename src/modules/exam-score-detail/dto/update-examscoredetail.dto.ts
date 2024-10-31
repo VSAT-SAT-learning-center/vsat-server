07e8197dde0c73246c178a0bad208299d1590034
Duy Nguyen Manh <108318095+DuyNM0601@users.noreply.github.com>
2024-10-31 23:14:29 +0700

    31/10/24-DuyNM Fix api get all exam score with detail

--- src/modules/exam-score-detail/dto/update-examscoredetail.dto.ts
+++ src/modules/exam-score-detail/dto/update-examscoredetail.dto.ts
@@ -1,23 +1,12 @@
-import { IsUUID, IsInt, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsUUID, IsInt, IsOptional, IsString } from 'class-validator';
 
 export class UpdateExamScoreDetailDto {
-  @IsUUID()
-  @IsOptional()
-  examScoreId?: string;
+    @IsInt()
+    @ApiProperty()
+    lowerscore?: number;
 
-  @IsUUID()
-  @IsOptional()
-  sectionId?: string;
-
-  @IsInt()
-  @IsOptional()
-  rawscore?: number;
-
-  @IsInt()
-  @IsOptional()
-  lowerscore?: number;
-
-  @IsInt()
-  @IsOptional()
-  upperscore?: number;
+    @IsInt()
+    @ApiProperty()
+    upperscore?: number;
 }
--- src/modules/exam-score-detail/exam-score-detail.service.ts
+++ src/modules/exam-score-detail/exam-score-detail.service.ts
@@ -5,6 +5,7 @@ import { CreateExamScoreDetailDto } from './dto/create-examscoredetail.dto';
 import { ExamScoreDetail } from 'src/database/entities/examscoredetail.entity';
 import { Section } from 'src/database/entities/section.entity';
 import { ExamScore } from 'src/database/entities/examscore.entity';
+import { UpdateExamScoreDetailDto } from './dto/update-examscoredetail.dto';
 
 @Injectable()
 export class ExamScoreDetailService {
@@ -19,30 +20,43 @@ export class ExamScoreDetailService {
         createExamScoreDetailsDto: CreateExamScoreDetailDto[],
         examScoreId: string,
     ): Promise<ExamScoreDetail[]> {
-        const sectionNames = createExamScoreDetailsDto.map(dto => dto.section);
+        const sectionNames = createExamScoreDetailsDto.map(
+            (dto) => dto.section,
+        );
         const sections = await this.sectionRepository.find({
             where: { name: In(sectionNames) },
         });
-        
-        const sectionMap = new Map(sections.map(section => [section.name, section]));
-    
-        const createdExamScoreDetails = createExamScoreDetailsDto.map(dto => {
+
+        const sectionMap = new Map(
+            sections.map((section) => [section.name, section]),
+        );
+
+        const createdExamScoreDetails = createExamScoreDetailsDto.map((dto) => {
             const section = sectionMap.get(dto.section);
-            
+
             if (!section) {
                 throw new NotFoundException(`Section ${dto.section} not found`);
             }
-    
+
             return this.examScoreDetailRepository.create({
                 ...dto,
                 section,
                 examScore: { id: examScoreId } as ExamScore,
             });
         });
-    
+
         return this.examScoreDetailRepository.save(createdExamScoreDetails);
     }
 
-    
-    
+    async update(id: string, updateExamScoreDetail: UpdateExamScoreDetailDto) {
+        const examScoreDetail = await this.examScoreDetailRepository.findOne({
+            where: { id: id },
+        });
+
+        if (!examScoreDetail) {
+            throw new NotFoundException(
+                `ExamScoreDetail with ID ${id} not found`,
+            );
+        }
+    }
 }
