9e42897b39cb6002c60d17b6a36ed252558a0fe6
Duy Nguyen Manh <108318095+DuyNM0601@users.noreply.github.com>
2024-11-01 00:09:19 +0700

    1/11/24-DuyNM Fix dto update examscore detail

--- src/modules/target-learning/target-learning.service.ts
+++ src/modules/target-learning/target-learning.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateTargetLearningDto } from './dto/create-targetlearning.dto';
@@ -9,14 +9,36 @@ import { BaseService } from '../base/base.service';
 
 @Injectable()
 export class TargetLearningService extends BaseService<TargetLearning> {
-  constructor(
-    @InjectRepository(TargetLearning)
-    private readonly targetLearningRepository: Repository<TargetLearning>,
-  ) {
-    super(targetLearningRepository);
-  }
+    constructor(
+        @InjectRepository(TargetLearning)
+        private readonly targetLearningRepository: Repository<TargetLearning>,
+    ) {
+        super(targetLearningRepository);
+    }
 
-  async save(){
-    
-  }
+    async save(createTargetLearningDto: CreateTargetLearningDto): Promise<CreateTargetLearningDto>{
+        const level = await this.targetLearningRepository.findOne({
+            where: { id: createTargetLearningDto.levelId },
+        });
+
+        const section = await this.targetLearningRepository.findOne({
+            where: { id: createTargetLearningDto.sectionId },
+        });
+
+        const studyProfile = await this.targetLearningRepository.findOne({
+            where: { id: createTargetLearningDto.studyProfileId },
+        });
+
+        if (!level) {
+            throw new NotFoundException('Level is not found');
+        }
+        if (!section) {
+            throw new NotFoundException('Section is not found');
+        }
+        if (!studyProfile) {
+            throw new NotFoundException('StudyProfile is not found');
+        }
+
+        return await this.targetLearningRepository.save(createTargetLearningDto);
+    }
 }
