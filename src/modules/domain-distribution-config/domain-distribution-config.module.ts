e32255790adf98f22d55159a5bdee2383206443b
Cong Cao <caocong2404@gmail.com>
2024-11-06 01:31:44 +0700

    5/11/24-CongCV2 Fix bug cannot update Domain

--- src/modules/domain-distribution-config/domain-distribution-config.module.ts
+++ src/modules/domain-distribution-config/domain-distribution-config.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DomainDistributionConfigController } from './domain-distribution-config.controller';
 import { DomainDistributionConfigService } from './domain-distribution-config.service';
+import { DomainDistributionConfig } from 'src/database/entities/domaindistributionconfig.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([DomainDistributionConfig])],
     providers: [DomainDistributionConfigService],
     controllers: [DomainDistributionConfigController],
 })
