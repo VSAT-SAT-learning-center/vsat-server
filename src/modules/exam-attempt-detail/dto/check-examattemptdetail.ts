1a2b627b75fe0fc2a433782207166d310428db58
Duy Nguyen Manh <108318095+DuyNM0601@users.noreply.github.com>
2024-11-12 23:25:53 +0700

    12/11/24-DuyNM Fix api get question

--- src/modules/exam-attempt-detail/dto/check-examattemptdetail.ts
+++ src/modules/exam-attempt-detail/dto/check-examattemptdetail.ts
@@ -4,11 +4,6 @@ import { IsString } from 'class-validator';
 
 export class CheckExamAttemptDetail {
     @Expose()
-    @ApiProperty({ example: '89404b38-e7b0-47c0-90f6-3be456cdfd56' })
-    @IsString()
-    examattemptid: string;
-
-    @Expose()
     @ApiProperty({ example: '59a25d07-c07c-4d4a-85eb-fa731942671e' })
     @IsString()
     questionid: string;
