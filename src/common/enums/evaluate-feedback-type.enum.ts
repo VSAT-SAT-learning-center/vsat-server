d04b12a89f42f7e669da6cb1bf4eb649bdbb10d2
Cong Cao <caocong2404@gmail.com>
2024-12-01 14:14:23 +0700

    1/12/24-CongCV2 Update notification dto

--- src/common/enums/evaluate-feedback-type.enum.ts
+++ src/common/enums/evaluate-feedback-type.enum.ts
@@ -1,7 +1,7 @@
 export enum EvaluateFeedbackType {  
-    STAFF_TO_MANAGER = 'STAFF_TO_MANAGER',
-    TEACHER_TO_STAFF = 'TEACHER_TO_STAFF',
-    TEACHER_TO_STUDENT = 'TEACHER_TO_STUDENT',
-    STUDENT_TO_TEACHER = 'STUDENT_TO_TEACHER',
-    STUDENT_TO_STAFF = 'STUDENT_TO_STAFF',
+    STAFF_TO_MANAGER = 'STAFF_TO_MANAGER', // send feedback
+    TEACHER_TO_STAFF = 'TEACHER_TO_STAFF', //send feedback
+    TEACHER_TO_STUDENT = 'TEACHER_TO_STUDENT', //send evaluate student
+    STUDENT_TO_TEACHER = 'STUDENT_TO_TEACHER', //send feedback
+    STUDENT_TO_STAFF = 'STUDENT_TO_STAFF', //send evaluate about teacher
 }
